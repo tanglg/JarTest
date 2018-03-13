3182feae9f31f17b5048391cc0c5f8925e82fb66
tanglg <tangligang@outlook.com>
2018-03-13 12:11:46 +0800

    #imp 支持计算基准价时传入标段编码

--- src/com/rhah/Main.java
+++ src/com/rhah/Main.java
@@ -22,20 +22,20 @@ public class Main {
     }
     private static   LinkedHashMap generateDemoBidderData(){
         LinkedHashMap<String, BigDecimal> bidderList = new LinkedHashMap<>();
-        bidderList.put("李32", BigDecimal.valueOf(80.0));
-        bidderList.put("张31", BigDecimal.valueOf(81.0));
-        bidderList.put("丁36", BigDecimal.valueOf(82.0));
-        bidderList.put("王33", BigDecimal.valueOf(83.0));
-        bidderList.put("赵34", BigDecimal.valueOf(100.0));
-        bidderList.put("孙35", BigDecimal.valueOf(101.0));
+        bidderList.put("李32", BigDecimal.valueOf(240000));
+        bidderList.put("张31", BigDecimal.valueOf(260000));
+        bidderList.put("丁36", BigDecimal.valueOf(260000));
+        bidderList.put("王33", BigDecimal.valueOf(83.7));
+        bidderList.put("赵34", BigDecimal.valueOf(100.1));
+        bidderList.put("孙35", BigDecimal.valueOf(101.5));
 
         return bidderList;
     }
     public static void testComputeBasePrice() throws ScriptException {
-        BasePrice priceObject= new BasePrice(generateDemoBidderData(),"H:\\zhaobiao\\示例项目\\双信封招标文件示例\\明珠花苑小学项目工程勘察设计.zbf");
+        BasePrice priceObject= new BasePrice(generateDemoBidderData(),"C:\\Users\\tanglg\\Desktop\\213\\213.zbf","I1300000001000288001004");
         BigDecimal basePrice = priceObject.getBasePrice();
         System.out.printf("最终计算基准价=%s%n",basePrice );
-        OfferScore score = new OfferScore(generateDemoBidderData(),"H:\\zhaobiao\\示例项目\\双信封招标文件示例\\明珠花苑小学项目工程勘察设计.zbf",basePrice);
+        OfferScore score = new OfferScore(generateDemoBidderData(),"C:\\Users\\tanglg\\Desktop\\213\\213.zbf",basePrice);
         LinkedHashMap offerScore = score.getBidderOfferScore();
         for (Object bidder : offerScore.keySet()
                 ) {

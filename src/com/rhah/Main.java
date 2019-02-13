42c559c063eeaf558c2ffa0999420b2b32c3fefb
tanglg <tangligang@outlook.com>
2019-02-13 18:04:38 +0800

    #fun 增加多报价计算支持

--- src/com/rhah/Main.java
+++ src/com/rhah/Main.java
@@ -16,31 +16,51 @@ public class Main {
         testComputeBasePrice();
         //TestSQLite testSQLite = new TestSQLite();
         //testSQLite.getSingleValueFromSqlite();
-        //RemoveBidderResult result = BasePrice .getRemvoeBidderCount("H:\\zhaobiao\\示例项目\\双信封招标文件示例\\明珠花苑小学项目工程勘察设计.zbf",3);
+        //RemoveBidderResult result = BasePrice .getRemvoeBidderCount("H:\zhaobiao\示例项目\EPC\epc项目测试",3);
         //System.out.printf("去除最高数量=%s%n", result.RemoveHeightCount);
         //System.out.printf("去除最低数量=%s%n", result.RemoveLowCount);
     }
-    private static   LinkedHashMap generateDemoBidderData(){
+    private static   LinkedHashMap generateDemoBidderOfferData(){
         LinkedHashMap<String, BigDecimal> bidderList = new LinkedHashMap<>();
-        bidderList.put("李32", BigDecimal.valueOf(240000));
-        bidderList.put("张31", BigDecimal.valueOf(260000));
-        bidderList.put("丁36", BigDecimal.valueOf(260000));
-        bidderList.put("王33", BigDecimal.valueOf(83.7));
-        bidderList.put("赵34", BigDecimal.valueOf(100.1));
-        bidderList.put("孙35", BigDecimal.valueOf(101.5));
+        bidderList.put("李32", BigDecimal.valueOf(41.5));
+        bidderList.put("张31", BigDecimal.valueOf(39.6));
+        bidderList.put("丁36", BigDecimal.valueOf(23));
+        bidderList.put("王33", BigDecimal.valueOf(35.19));
+        bidderList.put("赵34", BigDecimal.valueOf(31.1));
+        bidderList.put("孙35", BigDecimal.valueOf(37.5));
+
+        return bidderList;
+    }
+
+    private static   LinkedHashMap generateDemoBidderScoreData(){
+        LinkedHashMap<String, BigDecimal> bidderList = new LinkedHashMap<>();
+        bidderList.put("李32", BigDecimal.valueOf(90));
+        bidderList.put("张31", BigDecimal.valueOf(351000.0));
+        bidderList.put("丁36", BigDecimal.valueOf(68));
+        bidderList.put("王33", BigDecimal.valueOf(75.621));
+        bidderList.put("赵34", BigDecimal.valueOf(74.5));
+        bidderList.put("孙35", BigDecimal.valueOf(65.6));
 
         return bidderList;
     }
     public static void testComputeBasePrice() throws ScriptException {
-        BasePrice priceObject= new BasePrice(generateDemoBidderData(),"C:\\Users\\tanglg\\Desktop\\213\\213.zbf","I1300000001000288001004");
-        BigDecimal basePrice = priceObject.getBasePrice();
+        String zbfPath = "H:\\zhaobiao\\示例项目\\EPC\\epc项目测试\\epc项目测试.zbf";
+        /*BasePrice priceObject= new BasePrice(generateDemoBidderOfferData(),generateDemoBidderScoreData(),zbfPath,
+                "I1300000001000117001001","636856699991660264");*/
+
+        BasePrice priceObject= new BasePrice(zbfPath,"I1300000001000117001001","636761581219885840");
+        System.out.printf("基准价采用的报价条目=%s%n",priceObject.getPriceItem());
+        BigDecimal basePrice = priceObject.getBasePrice(generateDemoBidderOfferData(),generateDemoBidderScoreData(),3);
+
         System.out.printf("最终计算基准价=%s%n",basePrice );
-        OfferScore score = new OfferScore(generateDemoBidderData(),"C:\\Users\\tanglg\\Desktop\\213\\213.zbf",basePrice);
+
+        OfferScore score = new OfferScore(generateDemoBidderOfferData(),zbfPath,basePrice,"636856700322337313",2);
         LinkedHashMap offerScore = score.getBidderOfferScore();
         for (Object bidder : offerScore.keySet()
                 ) {
             System.out.printf("%s=%s%n", bidder, offerScore.get(bidder));
         }
+
     }
     public static void testComputeOfferScore() {
         //该测试数据库中，基准价采用的是平均值，报价得分=100，高1%减2分，低1%减1分，最高100分，最低0分
